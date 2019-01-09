0d1ec9788453b18948bd62204fb998678fff5930
gracekull21 <gracekull21@berkeley.edu>
2019-01-08 21:24:34 -0800

    Staged minutes in decade

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -3,3 +3,10 @@
 
 
 ### Your Code Here ###
+#assume two leap years in this decade (2010-2019, inclusive)
+leap_days = 366 * 2
+non_leap_days = 365 * 8
+total_days = leap_days + non_leap_days
+minutes_in_decade = total_days * 24 * 60
+minutes_in_decade
+#24 hrs in a day and 60 minutes in an hr.
