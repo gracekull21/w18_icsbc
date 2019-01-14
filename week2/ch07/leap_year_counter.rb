5632e983fe5c27713d792e8668b26886b54ed753
gracekull21 <gracekull21@berkeley.edu>
2019-01-13 23:28:07 -0800

    Finished leap year counter

--- week2/ch07/leap_year_counter.rb
+++ week2/ch07/leap_year_counter.rb
@@ -1,9 +1,31 @@
 # Remember, leap years are divisible by 4, but not by 100 unless also by 400
 
 ### Get input from user: ###
-print("Please enter a starting year: ")
+print "Please enter a starting year: "
 starting = gets.chomp ## FIXME?
-print("Now enter a ending year: ")
+#puts starting
+print "Now enter a ending year: "
 ending = gets.chomp ## FIXME?
+#puts ending
 
 ### Rest of your code here ###
+year = starting.to_i
+count = 0
+while year <= ending.to_i
+  if year % 4 != 0
+    count = count
+  else
+    if year % 100 == 0
+      if year % 400 == 0
+        count = count + 1
+        puts year
+      else
+        count = count
+      end
+    else
+      count = count + 1
+      puts year
+    end
+  end
+  year = year + 1
+end
