a710edd2ad2463a070f317e8bbdd441d6b466ae3
gracekull21 <gracekull21@berkeley.edu>
2019-01-14 00:20:08 -0800

    Finished deaf grandma extended.

--- week2/ch07/deaf_grandma_extended.rb
+++ week2/ch07/deaf_grandma_extended.rb
@@ -6,17 +6,14 @@ while bye_counter < 3 # Fill in the while condition
   if input == 'BYE'
     puts 'HUH?!  SPEAK UP, SONNY!'
     bye_counter = bye_counter + 1
-    puts bye_counter
   end
   if input == input.upcase && input != 'BYE'
     puts 'NO, NOT SINCE 1938!'
     bye_counter = 0
-    puts bye_counter
   end
   if input != input.upcase
     puts 'HUH?!  SPEAK UP, SONNY!'
     bye_counter = 0
-    puts bye_counter
   end
 end
 puts 'OK SONNY, TALK AGAIN SOON'
