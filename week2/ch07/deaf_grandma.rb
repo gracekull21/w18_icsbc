ae1fe7e181d20ab9641b0a9fcdc9ff79005be418
gracekull21 <gracekull21@berkeley.edu>
2019-01-13 22:34:52 -0800

    Finished deaf grandma

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,14 @@
 puts 'COME GIVE GRANNY A KISS!'
 
-while () # Fill in the while condition
+input = gets.chomp
+while input != 'BYE' # Fill in the while condition
   ### Your Code Here ###
+  if input == input.upcase
+    puts 'NO, NOT SINCE 1938!'
+  end
+  if input != input.upcase
+    puts 'HUH?!  SPEAK UP, SONNY!'
+  end
+  input = gets.chomp
 end
-
 puts 'OK SONNY, TALK AGAIN SOON'
