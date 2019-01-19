5a0c14641a49b7bdd3df9aac1c581290bebca01a
gracekull21 <gracekull21@berkeley.edu>
2019-01-18 18:31:44 -0800

    Finished factorial

--- week4/recursion_practice/factorial.rb
+++ week4/recursion_practice/factorial.rb
@@ -2,9 +2,11 @@
 # See if you can figure out how to write the method!
 
 def factorial(n)
-
-  # YOUR CODE HERE #
-
+  if n == 0
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
 end
 
 puts(factorial(3)) # expected: 6
